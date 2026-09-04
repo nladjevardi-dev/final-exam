050294a9af9c51d921a8b61f064ac14f91602f65
nladjevardi-dev <nladjevardi@student.scitech.edu>
2026-09-03 19:26:22 -0700

    Completed Final Exam project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const registerBtn = document.getElementById('register-btn');
+registerBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert("Thank you for registering");
+});
